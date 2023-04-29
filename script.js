91c8492509f72e958d9c471232172a0ac9267463
Ishan Rohatgi <ishan.77775@gmail.com>
2023-04-29 20:06:26 +0530

    removed cradles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function anime(number){
     let pendulum = document.querySelectorAll('.cradle-box .box');
-    for(let pen=0;pen<6;pen++){
+    for(let pen=0;pen<5;pen++){
         pendulum[pen].classList.remove('animate-left'); 
         pendulum[pen].classList.remove('animate-right');
     }
@@ -8,7 +8,7 @@ function anime(number){
         for(let pen=0;pen<number;pen++){
             pendulum[pen].classList.add('animate-left');
         }
-        for(let pen = 5;pen>5-number;pen--){
+        for(let pen = 4;pen>4-number;pen--){
             pendulum[pen].classList.add('animate-right');
         }
     },500);
